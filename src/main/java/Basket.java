8a7680c5e5920ca99417c5218511cc66e73fe37e
Sweta Shah <swetashah.8430@gmail.com>
2022-10-19 14:25:10 +0100

    Babs bagel finished

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -1,44 +1,44 @@
 import java.util.ArrayList;
 
 public class Basket {
-        static ArrayList<String> items = new ArrayList<String>(5);
-        static int basketCapacity = 5;
-    public static String add(String item){
+        ArrayList<String> items = new ArrayList<String>(5);
+         private int basketCapacity = 5;
+    public String add(String item){
         items.add(item);
         return items.get(0);
     }
-    public static void remove(String item){
+    public void remove(String item){
         items.remove(item);
 
     }
 
-    public static int changeBasketSize(int number){
+    public int changeBasketSize(int number){
      return basketCapacity += number;
     }
-    public static boolean isBasketFull(){
+    public boolean isBasketFull(){
         if(items.size()>basketCapacity){
             return true;
         }else{
             return false;
         }
     }
-    public static String isItemExist(String item){
+    public String isItemExist(String item){
         if(items.contains(item) == false){
            return "Item is not there to remove" ;
         }
         return "Item Exist";
     }
-    public static String isDuplicateItem(String item){
-        if(items.contains(item) == true){
+    public String isDuplicateItem(String item){
+        if(items.contains(item)){
             return "Item Exist";
         }
         items.add(item);
         return "Item is added to the Basket";
     }
-    public static int bagelPrice(Bagel item){
+    public int bagelPrice(Bagel item){
         return item.getPrice();
     }
-    public static int bagelTotalPrice (Bagel ...item) {
+    public int bagelTotalPrice (Bagel ...item) {
         int totalPrice = 0;
         for(Bagel b : item){
             totalPrice += b.getPrice();
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,33 +1,37 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BasketTest {
+    @BeforeEach
+    public void setup(){
+        Basket basket = new Basket();
+    }
+    @AfterEach
+    public void cleanBasket(){
+        basket.items.clear();
+    }
+    Basket basket = new Basket();
     @Test
     @DisplayName("Add item to Array")
     void add(){
         //Arrange
         String item1 = "Bagel";
         //Act
-        String result = Basket.add(item1);
+        String result = basket.add(item1);
         //Assert
         assertEquals(result, item1);
 
-    }
-
-    //clean up
-    @AfterEach
-    public void cleanBasket(){
-        Basket.items.clear();
     }
     @Test
     @DisplayName("Test length of Array when item is added")
     void checkSizeOfBasket(){
         String item1 = "Bagel";
         //Act
-         Basket.add(item1);
-         int result = Basket.items.size();
+        basket.add(item1);
+         int result = basket.items.size();
         //Assert
         assertEquals(result, 1);
 
@@ -37,22 +41,19 @@ public class BasketTest {
     void remove(){
         String item1 = "Bagel";
         //Act
-        Basket.remove(item1);
-        int result = Basket.items.size();
+        basket.remove(item1);
+        int result = basket.items.size();
         //Assert
         assertEquals(result, 0);
     }
     @Test
     @DisplayName("Changing Basket Size Capacity")
     void changeBasketSize(){
-
         int number = 2;
         //Act
-        Basket.changeBasketSize(number);
-        int result = Basket.basketCapacity;
+        int result = basket.changeBasketSize(number);
         //Assert
         assertEquals(result, 7);
-
     }
     @Test
     @DisplayName("Is Basket Full")
@@ -66,13 +67,13 @@ public class BasketTest {
         String item5 = "Bagel5";
         String item6 = "Bagel6";
         //Act
-        Basket.add(item1);
-        Basket.add(item2);
-        Basket.add(item3);
-        Basket.add(item4);
-        Basket.add(item5);
-        Basket.add(item6);
-        boolean result = Basket.isBasketFull();
+        basket.add(item1);
+        basket.add(item2);
+        basket.add(item3);
+        basket.add(item4);
+        basket.add(item5);
+        basket.add(item6);
+        boolean result = basket.isBasketFull();
         //Assert
         assertEquals(result, true);
     }
@@ -85,11 +86,11 @@ public class BasketTest {
         String item3 = "Bagel3";
         String item4 = "Bagel4";
         //Act
-        Basket.add(item1);
-        Basket.add(item2);
-        Basket.add(item3);
-        Basket.remove(item4);
-        String result =Basket.isItemExist(item4);
+        basket.add(item1);
+        basket.add(item2);
+        basket.add(item3);
+        basket.remove(item4);
+        String result =basket.isItemExist(item4);
         //Assert
         assertEquals(result, "Item is not there to remove");
     }
@@ -101,11 +102,11 @@ public class BasketTest {
         String item2 = "Bagel2";
         String item3 = "Bagel3";
         //Act
-        Basket.add(item1);
-        Basket.add(item2);
-        Basket.add(item3);
-        Basket.add(item3);
-        String result = Basket.isDuplicateItem(item3);
+        basket.add(item1);
+        basket.add(item2);
+        basket.add(item3);
+        basket.add(item3);
+        String result = basket.isDuplicateItem(item3);
         //Assert
         assertEquals(result, "Item Exist");
     }
@@ -115,7 +116,7 @@ public class BasketTest {
         //Arrange
         Bagel item1 = new Bagel("Bagel1",5);
         //Act
-        int result = Basket.bagelPrice(item1);
+        int result = basket.bagelPrice(item1);
         //Assert
         assertEquals(result, 5);
     }
@@ -128,7 +129,7 @@ public class BasketTest {
         Bagel item3 = new Bagel("Bagel3",5);
 
         //Act
-        int result = Basket.bagelTotalPrice(item1, item2, item3);
+        int result = basket.bagelTotalPrice(item1, item2, item3);
         //Assert
         assertEquals(result, 15);
     }
